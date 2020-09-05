12827b9605b17e739a6f1a53d98301db70ba74a3
Dietrich Friday <friday.dietrich@gmail.com>
2020-09-05 23:13:16 +0900

    Reworked ls aliases to use exa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,15 @@
 set fish_greeting # no greeting
 
 # Aliases. Work when just typing <RET> after them.
-alias la 'ls -a'
-alias l  'ls'
+alias l   'exa -F'
+alias la  'exa -a'
+alias ll  'exa -l --header --git'
+alias llt 'exa -l --header --git --tree' 
 
 # Abbreviations. Good for commands that will always be followed by a space.
 abbr g 'git'
 abbr v 'vi'
 abbr b 'bat'
-abbr cat 'bat'
 
 abbr cog 'git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # git for my config files
 
