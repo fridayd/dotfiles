64727648f87d39dda21f1618979a1fe68172390e
Dietrich Friday <friday.dietrich@gmail.com>
2020-09-05 11:47:10 +0900

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+alias l 'ls' # an alias, because I want to type 'l<RET>', not 'l<SPC><RET>'.
+
+abbr g 'git' # an abbr; 'git' is always followed by a space and arguments.
+
+abbr cog 'git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # git for my config files
