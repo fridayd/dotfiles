6ce240040cd7dc9da066f525084550a1f3f6064c
Dietrich Friday <friday.dietrich@gmail.com>
2020-09-07 14:29:13 +0900

    Adjusted fzf stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,14 @@ alias llt 'exa -l --header --git --tree'
 abbr g 'git'
 abbr v 'vi'
 abbr b 'bat'
+abbr pg 'ps xa|rg'
 
 abbr cog 'git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # git for my config files
 
+alias em 'emacsclient -n -c -a ""'
+
+set FZF_TMUX_HEIGHT 100%
+set FZF_DEFAULT_COMMAND "fd . $HOME"
+set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set FZF_ALT_C_COMMAND "fd -t d . $HOME"
+set FZF_DEFAULT_OPTS "--ansi --preview-window 'right:60%' --preview 'bat --color=always {}'"
